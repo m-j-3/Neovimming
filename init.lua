ee02974756c897ddbd43de5e06190f469f7630fc
m-j-3 <114370635+m-j-3@users.noreply.github.com>
2024-02-23 22:06:37 -0600

    idk

--- init.lua
+++ init.lua
@@ -1,5 +1,6 @@
 -- bootstrap lazy.nvim, LazyVim and your plugins
 require("config.lazy")
+
 require("neo-tree").setup({
   filesystem = {
     bind_to_cwd = true, -- true creates a 2-way binding between vim's cwd and neo-tree's root
